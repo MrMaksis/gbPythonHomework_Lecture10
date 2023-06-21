1d3ac77a064a49c4280f36bead25fbda3d70a2ad
mr_maksis <max.morink@yandex.ru>
2023-06-21 17:40:36 +0300

    The get_dummies function has been removed

--- homework_1.py
+++ homework_1.py
@@ -1,8 +1,16 @@
 import pandas as pd
 import random
+import numpy as np
+from sklearn.preprocessing import LabelEncoder
+
 lst = ['robot'] * 10
 lst += ['human'] * 10
 random.shuffle(lst)
+
+le = LabelEncoder()
+labels = le.fit_transform(lst)
+oh_labels = np.eye(2)[labels]
+
 data = pd.DataFrame({'whoAmI': lst})
-data_one_hot = pd.get_dummies(data, columns=['whoAmI'])
+data_one_hot = pd.DataFrame(oh_labels, columns=['whoAmI_0', 'whoAmI_1'])
 print(data_one_hot.head())
